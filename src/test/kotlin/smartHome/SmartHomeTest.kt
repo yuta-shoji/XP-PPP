48d73be26ce9030f5141608878c3e494ae0e3d07
mtonosaki <manabu@tomarika.com>
2022-10-04 17:02:09 +0900

    擬似コードのテストを日本語化

--- src/test/kotlin/smartHome/SmartHomeTest.kt
+++ src/test/kotlin/smartHome/SmartHomeTest.kt
@@ -9,7 +9,7 @@ class SmartHomeTest {
     // 実装：Misaki
 
     @Test
-    fun `Only turns bulb on when switch is On`() {
+    fun `スイッチONの状態の時は、バルブがONになる`() {
         // ARRANGE
         // バルブの状態を監視するスパイを作る
         // テストが実施できるように、常にONになる スイッチのスタブを作る
@@ -23,7 +23,7 @@ class SmartHomeTest {
     }
 
     @Test
-    fun `Only turns bulb off when switch is Off`() {
+    fun `スイッチOFFの時は、バルブがOFFになる`() {
         // ARRANGE
         // バルブの状態を監視するスパイを作る（上と同じ）
         // テストが実施できるように、常にOFFになる スイッチのスタブを作る
